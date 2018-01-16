3ac2a07af7722c4a15efc78102847e43e58507f4
Vanius <vanius@hotmail.com>
2018-01-16 16:21:38 -0200

    Interceptor para incluir token nas requisições

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,7 +28,7 @@ export class ProfilePage {
      this.clienteService.findByEmail(localUser.email)
          .subscribe(response => {
             this.cliente = response;
-            this.getImageIfExists();
+            this.getImageIfExists(); 
          },
         error => {} )
     } 
