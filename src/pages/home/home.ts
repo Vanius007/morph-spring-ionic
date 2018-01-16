d4b804d656d9302ef222eb1d2d809d5bbfdd2e89
Vanius <vanius@hotmail.com>
2018-01-16 10:55:00 -0200

    Salvando os dados do usuario logado no localstorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,7 +34,7 @@ export class HomePage {
 
     this.auth.authenticate(this.creds)
              .subscribe(response => {
-                console.log(response.headers.get('Authorization'))
+                this.auth.successfullLogin(response.headers.get('Authorization'))
                 this.navCtrl.setRoot('CategoriasPage');
              },
             error => {} )
